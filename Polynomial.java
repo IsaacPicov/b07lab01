d8f71a8e3f1c73a46b67021d3a187b0b80d0ad17
Isaac Picov <isaac.picov@gmail.com>
2024-09-29 21:15:18 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,8 +1,5 @@
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.HashMap;
+import java.util.*; 
+import java.io.*;
 
 public class Polynomial{
     //fields
@@ -27,84 +24,91 @@ public class Polynomial{
 
 
     public Polynomial(File file){
-        String poly = new Scanner(file);
-        String[] intermidiate = poly.split("-|\\+");
+       try{
+        Scanner poly = new Scanner(file);
+        String polyString = poly.nextLine();
+        poly.close();
+        String[] intermidiate = polyString.split("(?=[+-])");
         int [] exps = new int[intermidiate.length]; 
         double [] vals = new double[intermidiate.length]; 
         for(int i = 0; i < intermidiate.length; i++){
-            String toSplit = intermidiate[i];
-            if(toSplit.length() == 1){
-                vals[i] = parseDouble(toSplit);
+            String toSplit = intermidiate[i].trim();
+            if (toSplit.equals("-x")) {
+                vals[i] = -1;
+                exps[i] = 1;
+            } else if (toSplit.equals("+x")) {
+                vals[i] = 1;
+                exps[i] = 1;
+            } else if (toSplit.equals("-")) {
+                vals[i] = -1;
                 exps[i] = 0;
-            }
-            else{
-                String [] split = toSplit.split("");
-                vals[i] = parseDouble(split[0]);
-                exps[i] = parseInt(split[1]);
+            } else if (toSplit.equals("+")) {
+                vals[i] = 1;
+                exps[i] = 0;
+            } else if (!toSplit.contains("x")) {
+                vals[i] = Double.parseDouble(toSplit);
+                exps[i] = 0;
+            } else {
+                // Handle cases like "+3x2" or "-2x"
+                String[] split = toSplit.split("x");
+                
+                if (split[0].equals("+") || split[0].isEmpty()) {
+                    vals[i] = 1;
+                } else if (split[0].equals("-")) {
+                    vals[i] = -1;
+                } else {
+                    vals[i] = Double.parseDouble(split[0]);
+                }
+
+                if (split.length == 1 || split[1].isEmpty()) {
+                    exps[i] = 1; // "x" means x^1
+                } else {
+                    exps[i] = Integer.parseInt(split[1]);
+                }
             }
         }
-        HashMap<Integer, Double> sorting= new HashMap<>(); 
-        for(int i =0; i < vals.length; i++){
-            sorting.put(exp[i], vals[i]); 
+        
+        this.exp = exps;
+        this.coefficents = vals;
         }
-        exp = Arrays.sort(exps);
-        coefficents = new double[vals.length];
-        for(int i = 0; i < coefficents_copy.length; i++){
-            coefficents[i] = sorting.get(exp[i]); 
+        catch(FileNotFoundException e){
+            System.out.println("The file could not be found: " + e.getMessage());
         }
     }
-    //other methods
-   
-//store list of sums
-//store list of exponents 
+    //other methods 
 
-
-public Polynomial add(Polynomial poly2){
-    int [] poly2_exp = poly2.exp;
-    double [] poly2_co = poly2.coefficents;
-    max1 = get_biggest(exp);
-    max2= get_biggest(poly2_exp);
-    max = math.max(max1, max2) +1; 
-    ArrayList<Double> sum = new ArrayList<Double>(); 
-    for(int i = 0; i < max; i++){
-        sum.add(0); 
-    }
+public Polynomial add (Polynomial poly2){
+    HashMap<Integer, Double> addMap = new HashMap<>();
     for(int i = 0; i < exp.length; i++){
-        sum.set(exp[i], sum.get(exp[i]) + coefficents[i]);
+        addMap.put(exp[i], coefficents[i]);
     }
-    for(int i = 0; i< poly2_exp.length; i++){
-        sum.set(poly2_exp[i], sum.get(poly2_exp[i])+ poly2_co[i]);
-    }
-    //combining arrays
-    ArrayList<Integer> allExp = new ArrayList<>(Arrays.asList(poly2_exp)); 
-    for(int i = 0; i< exp.length; i++){
-        if(!allExp.contains(exp[i])){
-            allExp.add(exp[i]);
+    for(int i = 0; i < poly2.exp.length;i++){
+        if(addMap.containsKey(poly2.exp[i])){
+            double val = addMap.get(poly2.exp[i]);
+            addMap.remove(poly2.exp[i]);
+            addMap.put(poly2.exp[i], val+poly2.coefficents[i]);
         }
     }
-    allExp.sort(null); 
-    int [] allExpArr = allExp.toArray();
-    double [] finalSum = new double[allExpArr.length];
-    for(int i = 0; i < allExpArr.length; i++){
-        finalSum[i] = sum.get(allExpArr[i]); 
-    }
-    return new Polynomial(finalSum, allExpArr); 
-    }
-    
-
-
-private int get_biggest(int [] exp){
-        int max = exp[0];
-        for(int i = 0; i < exp.length;i++){
-            if(exp[i] > max){
-                max = exp[i];
-            }
+    ArrayList<Integer> expArrayList = new ArrayList<>();
+    ArrayList<Double> coArrayList = new ArrayList<>();
+    for(Map.Entry<Integer, Double> entry: addMap.entrySet()){
+        if (!(entry.getValue()).equals(0)){
+            expArrayList.add(entry.getKey());
+            coArrayList.add(entry.getValue()); 
         }
-        return max;
     }
+    Integer [] integerExp = expArrayList.toArray(new Integer[0]);
+    Double [] doubleCo = coArrayList.toArray(new Double[0]);
+    int[] intExp = new int[integerExp.length];
+    double [] realCo = new double[doubleCo.length];
+    for(int i = 0; i<integerExp.length;i++){
+        intExp[i] = integerExp[i];
+        realCo[i] = doubleCo[i];
+    }
+    return new Polynomial(realCo, intExp);
+}
 
 
-//order doesn't matter
 public double evaluate(double x){
         double sum = 0;
         for (int i = 0; i < coefficents.length; i++){
@@ -114,18 +118,16 @@ public double evaluate(double x){
     }
 
 
-//order doesn't matter
 public boolean hasRoot(double x){
         return(evaluate(x) == 0); 
     }
 
-//order matters in the return statement
 public Polynomial multiply (Polynomial poly){
         HashMap <Integer, Double> result = new HashMap<>();
-        for (i = 0; i < exp.length; i++){
+        for (int i = 0; i < exp.length; i++){
             int deg = exp[i]; 
             double val = coefficents[i];  
-            for (j = 0; j < poly.exp.length; j++){
+            for (int j = 0; j < poly.exp.length; j++){
                 int poly2Deg = poly.exp[j]; 
                 double poly2Coeff = poly.coefficents[j];
                 deg += poly2Deg;
@@ -133,46 +135,52 @@ public Polynomial multiply (Polynomial poly){
             }
             result.put(deg, val); 
         }
-        ArrayList<Integer> exps = new ArrayList<>(Arrays.asList(keys.result));
-        exps.sort(null); 
-        Double [] co = new Double[exps.size()];
-        for(int i = 0; i < co.length; i++){
-            co[i] = result.get(exps.get(i)); 
+        ArrayList<Integer> expArrayList = new ArrayList<>();
+        ArrayList<Double> coArrayList = new ArrayList<>();
+        for(Map.Entry<Integer, Double> entry: result.entrySet()){
+            if (!(entry.getValue()).equals(0)){
+                expArrayList.add(entry.getKey());
+                coArrayList.add(entry.getValue()); 
+            }
         }
-        int [] expsArr = exps.toArray(); 
-        return new Polynomial(co, expsArr); 
+        Integer [] integerExp = expArrayList.toArray(new Integer[0]);
+        Double [] doubleCo = coArrayList.toArray(new Double[0]);
+        int[] intExp = new int[integerExp.length];
+        double [] realCo = new double[doubleCo.length];
+        for(int i = 0; i<integerExp.length;i++){
+            intExp[i] = integerExp[i];
+            realCo[i] = doubleCo[i];
+        }
+        return new Polynomial(realCo, intExp);
     }
+
 
 
 //order matters in the input 
 public void saveToFile(String fileName){
         String poly = "";
-        HashMap<Integer, Double> sorting= new HashMap<>(); 
-        for(int i =0; i < coefficents.length; i++){
-            sorting.put(exp[i], coefficents[i]); 
-        }
-        double [] coefficents_copy = new double[coefficents.length];
-        int [] exp_copy = Arrays.sort(exp);
-        for(int i = 0; i < coefficents_copy.length; i++){
-            coefficents_copy[i] = sorting.get(exp_copy[i]); 
-        }
-        for(i = 0; i < exp.length; i++){
-            if(exp_copy[i] == 0){
-                poly = String(coefficents_copy[i]);
+        for(int i = 0; i < exp.length; i++){
+            if(exp[i] == 0){
+                poly = String.valueOf(coefficents[i]);
             }
-            else if(poly.length() == 0 & coefficents_copy[i] > 0){
-                poly = String(coefficents_copy[i]) + 'x' + String(exp_copy[i]);
+            else if(poly.length() == 0 & coefficents[i] > 0){
+                poly = String.valueOf(coefficents[i]) + 'x' + String.valueOf(exp[i]);
             }
-            else if(coefficents_copy[i] < 0){
-                poly += '-' + String(coefficents_copy[i]) + 'x' + String(exp_copy[i]);
+            else if(coefficents[i] < 0){
+                poly += '-' + String.valueOf(coefficents[i]) + 'x' + String.valueOf(exp[i]);
             }
             else{
-                poly += '+' + String(coefficents_copy[i]) + 'x' + String(exp_copy[i]);
+                poly += '+' + String.valueOf(coefficents[i]) + 'x' + String.valueOf(exp[i]);
             }
         }
-        FileWriter myFile = new FileWriter("%s",false, fileName);
+        try{
+        FileWriter myFile = new FileWriter(fileName,false);
         myFile.write(poly);
         myFile.close(); 
+        }
+        catch(IOException e){
+            System.out.println("An I/O error occurred: " + e.getMessage());
+        }
         
     }
 }
