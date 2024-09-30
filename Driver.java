db98a7ff936223f38828ceb7a5295d4cb3576e7d
Isaac Picov <104283077+IsaacPicov@users.noreply.github.com>
2024-09-29 21:22:15 -0400

    Revert "Lab2"

--- Driver.java
+++ Driver.java
@@ -1,75 +1,16 @@
-import java.io.File;
-import java.io.IOException;
-
 public class Driver {
-    public static void test_normalPolynomial() {
+    public static void main(String [] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,5};
-        int [] e1 = {1, 4};
-        Polynomial p1 = new Polynomial(c1, e1);
-        double [] c2 = {-2,-9, 10};
-        int [] e2 = {1, 3, 5};
-        Polynomial p2 = new Polynomial(c2, e2);
+        double [] c1 = {6,0,0,5};
+        Polynomial p1 = new Polynomial(c1);
+        double [] c2 = {0,-2,0,0,-9};
+        Polynomial p2 = new Polynomial(c2);
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(-1))
-            System.out.println("-1 is a root of s");
+        if(s.hasRoot(1))
+        System.out.println("1 is a root of s");
         else
-            System.out.println("-1 is not a root of s");
-    }
-
-    public static void test_filePolynomial() throws IOException {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        File poly1 = new File("poly1.txt");
-        File poly2 = new File("poly2.txt");
-        Polynomial p1 = new Polynomial(poly1);
-        Polynomial p2 = new Polynomial(poly2);
-        Polynomial s = p1.add(p2);
-        Polynomial s2 = p2.add(p1);
-        // s.printPolynomial();
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        System.out.println("s(4) = " + s.evaluate(4));
-        System.out.println("s2(0.1) = " + s2.evaluate(0.1));
-        System.out.println("s2(4) = " + s2.evaluate(4));
-
-        Polynomial mul = p1.multiply(p2);
-        System.out.println("mul(0.1) = " + mul.evaluate(0.1));
-        System.out.println("mul(3) = " + mul.evaluate(3));
-        if(mul.hasRoot(1))
-            System.out.println("1 is a root of mul");
-        else
-            System.out.println("1 is not a root of mul");
-
-        mul.saveToFile("result.txt");
-        File poly3 = new File("result.txt");
-        Polynomial p3 = new Polynomial(poly3);
-        System.out.println("p3(0.1) = " + p3.evaluate(0.1));
-        System.out.println("p3(3) = " + p3.evaluate(3));
-        if(p3.hasRoot(1))
-            System.out.println("1 is a root of mul");
-        else
-            System.out.println("1 is not a root of mul");
-    }
-
-    public static void test_edgePolynomial() throws IOException {
-        // double [] coef = { 0, 1, 0, -4, 0, 5 };
-        // int [] expo = { 1, 3, 5, 4, 6, 9 };
-        // Polynomial p1 = new Polynomial(coef, expo);
-        // p1.saveToFile("result.txt");
-
-        double [] coef1 = { 0, 0, 0, 0, 0, 0 };
-        int [] expo1 = { 1, 3, 5, 4, 6, 9 };
-        Polynomial p2 = new Polynomial(coef1, expo1);
-        p2.saveToFile("result.txt");
-
-        
-    }
-
-    public static void main(String [] args) throws IOException {
-        // test_normalPolynomial();
-        test_filePolynomial();
-        // test_edgePolynomial();
+        System.out.println("1 is not a root of s");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,186 +1,42 @@
-import java.util.*; 
-import java.io.*;
-
 public class Polynomial{
     //fields
     public double [] coefficents;
-    public int [] exp; 
     //methods
-    //constructors
-
-
     public Polynomial(){
-        coefficents = new double[]{0}; 
-        exp = new int[]{0}; 
+        coefficents = new double[]{0};
     }
-
-
-
-    public Polynomial(double [] coefficents, int [] exp){
-        this.coefficents = coefficents;
-        this.exp = exp; 
+    public Polynomial(double [] c){
+        coefficents = c;
     }
-
-
-
-    public Polynomial(File file){
-       try{
-        Scanner poly = new Scanner(file);
-        String polyString = poly.nextLine();
-        poly.close();
-        String[] intermidiate = polyString.split("(?=[+-])");
-        int [] exps = new int[intermidiate.length]; 
-        double [] vals = new double[intermidiate.length]; 
-        for(int i = 0; i < intermidiate.length; i++){
-            String toSplit = intermidiate[i].trim();
-            if (toSplit.equals("-x")) {
-                vals[i] = -1;
-                exps[i] = 1;
-            } else if (toSplit.equals("+x")) {
-                vals[i] = 1;
-                exps[i] = 1;
-            } else if (toSplit.equals("-")) {
-                vals[i] = -1;
-                exps[i] = 0;
-            } else if (toSplit.equals("+")) {
-                vals[i] = 1;
-                exps[i] = 0;
-            } else if (!toSplit.contains("x")) {
-                vals[i] = Double.parseDouble(toSplit);
-                exps[i] = 0;
-            } else {
-                // Handle cases like "+3x2" or "-2x"
-                String[] split = toSplit.split("x");
-                
-                if (split[0].equals("+") || split[0].isEmpty()) {
-                    vals[i] = 1;
-                } else if (split[0].equals("-")) {
-                    vals[i] = -1;
-                } else {
-                    vals[i] = Double.parseDouble(split[0]);
-                }
-
-                if (split.length == 1 || split[1].isEmpty()) {
-                    exps[i] = 1; // "x" means x^1
-                } else {
-                    exps[i] = Integer.parseInt(split[1]);
-                }
-            }
+    public Polynomial add(Polynomial poly){
+        double [] coefficents1 = coefficents;
+        double [] coefficents2= poly.coefficents;
+        int len;
+        double [] smaller;
+        double [] bigger;
+        if(coefficents1.length> coefficents2.length){
+            len = coefficents1.length -1;
+            bigger = coefficents1;
+            smaller = coefficents2;
         }
-        
-        this.exp = exps;
-        this.coefficents = vals;
+        else{
+             len = coefficents2.length -1;
+             bigger = coefficents2;
+             smaller = coefficents1;
         }
-        catch(FileNotFoundException e){
-            System.out.println("The file could not be found: " + e.getMessage());
+        for (int i = 0; i < len; i++){
+            bigger[i] = bigger[i]+smaller[i];
         }
+        return new Polynomial(bigger); 
     }
-    //other methods 
-
-public Polynomial add (Polynomial poly2){
-    HashMap<Integer, Double> addMap = new HashMap<>();
-    for(int i = 0; i < exp.length; i++){
-        addMap.put(exp[i], coefficents[i]);
-    }
-    for(int i = 0; i < poly2.exp.length;i++){
-        if(addMap.containsKey(poly2.exp[i])){
-            double val = addMap.get(poly2.exp[i]);
-            addMap.remove(poly2.exp[i]);
-            addMap.put(poly2.exp[i], val+poly2.coefficents[i]);
-        }
-    }
-    ArrayList<Integer> expArrayList = new ArrayList<>();
-    ArrayList<Double> coArrayList = new ArrayList<>();
-    for(Map.Entry<Integer, Double> entry: addMap.entrySet()){
-        if (!(entry.getValue()).equals(0)){
-            expArrayList.add(entry.getKey());
-            coArrayList.add(entry.getValue()); 
-        }
-    }
-    Integer [] integerExp = expArrayList.toArray(new Integer[0]);
-    Double [] doubleCo = coArrayList.toArray(new Double[0]);
-    int[] intExp = new int[integerExp.length];
-    double [] realCo = new double[doubleCo.length];
-    for(int i = 0; i<integerExp.length;i++){
-        intExp[i] = integerExp[i];
-        realCo[i] = doubleCo[i];
-    }
-    return new Polynomial(realCo, intExp);
-}
-
-
-public double evaluate(double x){
-        double sum = 0;
-        for (int i = 0; i < coefficents.length; i++){
-            sum += coefficents[i]*(Math.pow(x,exp[i]));
+    public double evaluate(double x){
+        double sum = coefficents[0];
+        for (int i = 1; i < coefficents.length; i++){
+            sum += coefficents[i]*(Math.pow(x,i));
         }
         return sum;
     }
-
-
-public boolean hasRoot(double x){
+    public boolean hasRoot(double x){
         return(evaluate(x) == 0); 
-    }
-
-public Polynomial multiply (Polynomial poly){
-        HashMap <Integer, Double> result = new HashMap<>();
-        for (int i = 0; i < exp.length; i++){
-            int deg = exp[i]; 
-            double val = coefficents[i];  
-            for (int j = 0; j < poly.exp.length; j++){
-                int poly2Deg = poly.exp[j]; 
-                double poly2Coeff = poly.coefficents[j];
-                deg += poly2Deg;
-                val *= poly2Coeff; 
-            }
-            result.put(deg, val); 
-        }
-        ArrayList<Integer> expArrayList = new ArrayList<>();
-        ArrayList<Double> coArrayList = new ArrayList<>();
-        for(Map.Entry<Integer, Double> entry: result.entrySet()){
-            if (!(entry.getValue()).equals(0)){
-                expArrayList.add(entry.getKey());
-                coArrayList.add(entry.getValue()); 
-            }
-        }
-        Integer [] integerExp = expArrayList.toArray(new Integer[0]);
-        Double [] doubleCo = coArrayList.toArray(new Double[0]);
-        int[] intExp = new int[integerExp.length];
-        double [] realCo = new double[doubleCo.length];
-        for(int i = 0; i<integerExp.length;i++){
-            intExp[i] = integerExp[i];
-            realCo[i] = doubleCo[i];
-        }
-        return new Polynomial(realCo, intExp);
-    }
-
-
-
-//order matters in the input 
-public void saveToFile(String fileName){
-        String poly = "";
-        for(int i = 0; i < exp.length; i++){
-            if(exp[i] == 0){
-                poly = String.valueOf(coefficents[i]);
-            }
-            else if(poly.length() == 0 & coefficents[i] > 0){
-                poly = String.valueOf(coefficents[i]) + 'x' + String.valueOf(exp[i]);
-            }
-            else if(coefficents[i] < 0){
-                poly += '-' + String.valueOf(coefficents[i]) + 'x' + String.valueOf(exp[i]);
-            }
-            else{
-                poly += '+' + String.valueOf(coefficents[i]) + 'x' + String.valueOf(exp[i]);
-            }
-        }
-        try{
-        FileWriter myFile = new FileWriter(fileName,false);
-        myFile.write(poly);
-        myFile.close(); 
-        }
-        catch(IOException e){
-            System.out.println("An I/O error occurred: " + e.getMessage());
-        }
-        
     }
 }
